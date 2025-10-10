7181c742b3b0bb1993a2308a0560defdcd34e9b2
Gerard Anderias <gjanderias@ucdavis.edu>
2025-10-10 14:47:57 -0700

    Add env dependency

--- HW1/src/main/java/com/ecs160/hw/App.java
+++ HW1/src/main/java/com/ecs160/hw/App.java
@@ -1,6 +1,11 @@
 package com.ecs160.hw;
 
 import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 /**
@@ -10,6 +15,11 @@ import java.util.List;
 public class App 
 {
     public static void main( String[] args ) {
+
+        Dotenv dotenv = Dotenv.load();
+        String token=dotenv.get("github_API_KEY");
+        System.out.println("environment variable: " + token);
+
         
     }
 }
